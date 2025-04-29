672abd8fa3293fc7d761ed669619b67d5df4f926
mindahun21 <mndahun0@gmail.com>
2025-04-30 00:15:17 +0300

    fix: cors option for deploying

--- pulcity/settings.py
+++ pulcity/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+from corsheaders.defaults import default_headers
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,10 +35,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -158,11 +159,11 @@ REST_FRAMEWORK = {
 
 
 CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "https://mindahun.pro.et",
-#     "http://localhost:5173",
-# ]
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'content-type',
+    'authorization',
+]
+
 
 EMAIL_BACKEND = config('EMAIL_BACKEND')
 EMAIL_HOST = config('EMAIL_HOST')  
