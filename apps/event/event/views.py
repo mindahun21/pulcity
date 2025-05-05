0907b2c4cf6c5d4a5d389771363c075aadfbc668
mindahun21 <mndahun0@gmail.com>
2025-05-05 17:14:00 +0300

    feat: add event/user/tickets endpoint

--- apps/event/event/views.py
+++ apps/event/event/views.py
@@ -4,7 +4,7 @@ from rest_framework import(
 )
 from rest_framework.response import Response
 from apps.event.serializers import EventSerializer
-from apps.event.models import Event, Ticket
+from apps.event.models import Event, Ticket, UserTicket
 from apps.event.ticket.serializers import TicketSerializer
 from commons.permisions import IsOrganization
 from drf_spectacular.utils import extend_schema
@@ -30,6 +30,25 @@ class EventViewSet(viewsets.ModelViewSet):
     tickets = Ticket.objects.filter(event=event)
     serializer = TicketSerializer(tickets,many=True)
     return Response({"tickets": serializer.data})
+  
+  
+  @extend_schema(
+    description="Retreve all tickets a user bought for an event specified by the parameter id.",
+    responses=TicketSerializer(many=True)
+  )
+  @action(detail=True, methods=['get'], url_path='user/tickets')
+  def user_tickets(self, request, id=None):
+    event = self.get_object()
+    user = request.user
+
+    user_tickets = UserTicket.objects.filter(
+      user=user,
+      ticket__event=event
+    ).select_related('ticket')
+    tickets = [ut.ticket for ut in user_tickets]
+    serializer = TicketSerializer(tickets, many=True)
+    return Response(serializer.data)
+    
     
   
   
