31424a76e8738fa3df1b9d7eb1085846c02453d3
mindahun21 <mndahun0@gmail.com>
2025-05-06 20:44:50 +0300

    feat: making access token expiration one day

--- pulcity/settings.py
+++ pulcity/settings.py
@@ -84,7 +84,7 @@ from datetime import timedelta
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
 
     'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
 
